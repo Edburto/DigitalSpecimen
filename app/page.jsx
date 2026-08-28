95689e023c0183d7f6d72d09114d52e7d1cacd11
Edburto <edgar.burbano.eb@gmail.com>
2026-08-28 17:19:51 +1000

    Add Section3Manifesto to the Page component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,13 @@
 'use client';
 
 import Section2SoilCutaway from '../components/Section2SoilCutaway';
+import Section3Manifesto from '../components/Section3Manifesto';
 
 export default function Page() {
-  return <Section2SoilCutaway />;
+  return (
+    <>
+      <Section2SoilCutaway />
+      <Section3Manifesto />
+    </>
+  );
 }
