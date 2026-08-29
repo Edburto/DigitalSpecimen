4901ab9b8453991c458dd723f6f581bc06af687d
Edburto <edgar.burbano.eb@gmail.com>
2026-08-29 15:10:21 +1000

    Add Footer component to Page

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@
 import Section2SoilCutaway from '../components/Section2SoilCutaway';
 import Section3Manifesto from '../components/Section3Manifesto';
 import Section4Taxonomy from '../components/Section4Taxonomy';
+import Footer from '../components/Footer';
+// ...
 
 export default function Page() {
   return (
@@ -10,6 +12,7 @@ export default function Page() {
       <Section2SoilCutaway />
       <Section3Manifesto />
       <Section4Taxonomy />
+      <Footer />
     </>
   );
 }
