96ec1368d262304c75526552e64e6e63ad92a5a2
Edburto <edgar.burbano.eb@gmail.com>
2026-08-29 14:19:19 +1000

    Add Section4Taxonomy component to Page

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,14 @@
 
 import Section2SoilCutaway from '../components/Section2SoilCutaway';
 import Section3Manifesto from '../components/Section3Manifesto';
+import Section4Taxonomy from '../components/Section4Taxonomy';
 
 export default function Page() {
   return (
     <>
       <Section2SoilCutaway />
       <Section3Manifesto />
+      <Section4Taxonomy />
     </>
   );
 }
